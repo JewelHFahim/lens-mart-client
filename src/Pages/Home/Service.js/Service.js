46b7a6073b6d058ec58cc7a7d0e8ec60105fba0a
JewelHFahim <jewelhfahim@gmail.com>
2022-11-24 03:14:38 +0600

    home and categories

---
+++ src/Pages/Home/Service.js/Service.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Service = () => {
+  return (
+    <div>
+      <div className="hero bg-base-200 py-4 mb-10">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <p className="py-2 text-lg">Service Area</p>
+            <h1 className="text-4xl font-bold">EXPLORE CATEGORIES</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
